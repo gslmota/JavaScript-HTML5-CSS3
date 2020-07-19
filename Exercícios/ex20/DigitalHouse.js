eda1035d64f572d9a85beeca20b06a28f1f4aa70
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-19 19:54:56 -0300

    Update

--- Exercícios/ex20/DigitalHouse.js
+++
@@ -1,8 +0,0 @@
-function autonomia(quantidadeDeCombustivel, rendimento){
-    var autonomia = rendimento * quantidadeDeCombustivel
-    return autonomia
-}
-
-// Programa principal
-var d = autonomia(34, 4)
-console.log(d)
