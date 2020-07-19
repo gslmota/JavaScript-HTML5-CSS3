f231238f3fe8c79fb494f67459caae44ecba5116
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-19 20:45:17 -0300

    Update

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -7,6 +7,10 @@ function maisBaratosQue(valor, precos) {
  }
  
  function precosEntre(valorMenor, valorMaior, precos){
-    var vm = +maisCarosQue(valorMenor, precos) + maisBaratosQue(valorMaior, precos) 
+    var vm = maisBaratosQue(valorMenor, precos) && maisCarosQue(valorMenor, precos)
     return vm
  }
+
+
+ var c = precosEntre(5, 10, [ 1,2,3,4,5,6,7,8,9,10])
+ console.log(c)
