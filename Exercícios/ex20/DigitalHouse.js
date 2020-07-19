9c8962cef0105485572f1d9f6ad203617a4fa384
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-19 20:24:53 -0300

    U´pdare

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -1,26 +1,8 @@
-// A função a seguir (calculaIdade) não precisa ser alterada
-// A sua função deve ser escrita logo abaixo desta
-function calcularIdade(dataDeNascimento) {
-    var [dia, mes, ano] = dataDeNascimento.split('/');
-    var d = new Date();
-    var anoAtual = d.getFullYear();
-    var mesAtual = d.getMonth() + 1;
-    var diaAtual = d.getDate();
-    ano = +ano; mes = +mes; dia = +dia;
-    var quantosAnos = anoAtual - ano;
-    if (mesAtual < mes || mesAtual == mes && diaAtual < dia) {
-        quantosAnos--;
-    }
-    return quantosAnos < 0 ? 0 : quantosAnos;
+function maisBaratosQue(valor, precos) {
+    return precos.filter(p => p <= valor);
  }
   
- // Escreva aqui sua função
-function deixaEntrar(dataDeNascimento, censura) {
-    var nsc = calcularIdade(dataDeNascimento)
-    if(nsc > censura){
-        return true
-    }
-    else{
-        return false
-    }
-} 
+ function maisCarosQue(valor, precos){
+    return precos.filter(p => p >= valor);
+ }
+ 
