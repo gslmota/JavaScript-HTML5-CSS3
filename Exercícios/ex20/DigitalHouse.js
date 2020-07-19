768b80e07f0704ae8df0913dd3b9b2e317df2f76
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-19 20:24:11 -0300

    Update

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -1,6 +1,26 @@
-// Seu código vem aqui. Lembre-se: a função validaCPF já está definida.
-if(validaCPF(576.524.020-85)){
-    console.log("CPF válido")
-}else{
-    console.log("CPF inválido")
-}
+// A função a seguir (calculaIdade) não precisa ser alterada
+// A sua função deve ser escrita logo abaixo desta
+function calcularIdade(dataDeNascimento) {
+    var [dia, mes, ano] = dataDeNascimento.split('/');
+    var d = new Date();
+    var anoAtual = d.getFullYear();
+    var mesAtual = d.getMonth() + 1;
+    var diaAtual = d.getDate();
+    ano = +ano; mes = +mes; dia = +dia;
+    var quantosAnos = anoAtual - ano;
+    if (mesAtual < mes || mesAtual == mes && diaAtual < dia) {
+        quantosAnos--;
+    }
+    return quantosAnos < 0 ? 0 : quantosAnos;
+ }
+  
+ // Escreva aqui sua função
+function deixaEntrar(dataDeNascimento, censura) {
+    var nsc = calcularIdade(dataDeNascimento)
+    if(nsc > censura){
+        return true
+    }
+    else{
+        return false
+    }
+} 
