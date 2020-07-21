0f7ada8a8e784870e10afe6447d4becfb6c9ecf3
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-21 20:59:04 -0300

    Up

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -1,4 +1,4 @@
-function trasnformaParaMaiusculo(arrayPalavras){
+function transformaParaMaiusculo(arrayPalavras){
    for(var i = 0;i<=arrayPalavras.length;i++){
       arrayPalavras[i].push(arrayPalavras[i].toUpperCase())
    }
