767b763e238c6d5a4e40f025d4107c6aa2561a52
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-20 21:04:01 -0300

    fim

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -2,5 +2,7 @@ function estavaPresenteNaAula(nomeDoAluno, nomesDosPresentes){
   var c = nomesDosPresentes.indexOf(nomeDoAluno)
   if(c == -1){
      return false
-  } es
+  }else {
+     return true
+  }
 }
