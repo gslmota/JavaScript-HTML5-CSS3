af3604725fc1b408b2ee3fe9a678d949df0ad637
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-22 19:21:30 -0300

    Up

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -14,3 +14,17 @@ Seu trabalho é pegar cada valor do array alunos e colocar o valor correspondent
 var alunos = [170, 159, 151, 187, 156, 191, 165, 154, 167, 169, 171, 170, 160]
 
 var grupoA = []
+var grupoB = []
+var grupoC = []
+for(var i=0;i<alunos.length;i++){
+   if(alunos[i]>=150 && alunos[i]<=159){
+      grupoA.push(alunos[i])
+   }
+   else if(alunos[i]>=160 && alunos[i]<=169){
+      grupoB.push(alunos[i])
+   }
+   else{
+      grupoC.push(alunos[i])
+
+   }
+}
