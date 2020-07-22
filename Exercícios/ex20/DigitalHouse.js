946847c03322aaef0a481a56e57dcd7cb434825d
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-21 21:17:41 -0300

    Up

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -1,7 +1,10 @@
-function gerarDezenas(){
-   var ms = []
-   for(var i = 1; i<=6;i++){
-      ms[i] =Math.round(Math.random() * 60)
+function maxmin(a, b, c, d, e){
+   var z = [a, b, c, d, e]
+   var maior = Math.max(z)
+   var menor = Math.min(z)
+   for(var i = 0; i<5;i++){
+      if(z[i] == maior){
+
+      }
    }
-   return ms
 }
