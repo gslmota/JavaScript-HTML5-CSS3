a0d6855dfada7020fee9771e8909b8f566b05fb1
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-21 21:22:10 -0300

    Up

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -4,7 +4,17 @@ function maxmin(a, b, c, d, e){
    var menor = Math.min(z)
    for(var i = 0; i<5;i++){
       if(z[i] == maior){
-
+         var x = i
+      }
+      else if(z[i] == menor){
+         var z = i
       }
    }
+   var t = a
+   z[0] = menor
+   z[z] = t
+   var k = b
+   z[1] = maior
+   z[x] = k
+   return z
 }
