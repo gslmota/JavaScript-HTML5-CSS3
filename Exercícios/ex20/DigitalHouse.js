576d5392aa2b52226057d8791b95d160a98208b1
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-22 20:30:07 -0300

    Update

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -1,13 +1,33 @@
-function transformaParaMaiusculo(palavras) {  
+/*Programar uma função filme que recebe três arrays com os nomes de personagens, filmes e ano de estréia no cinema. A função deve receber também um valor de id escolhido pelo usuário com intervalo de 1 até o tamanho máximo dos arrays fornecidos e retornar uma frase com o seguinte modelo: "personagem é um personagem do filme filme que estreou no cinema em lançamento." Se o valor de id for inválido, a função deve retornar a frase "Essa não é uma opção válida."
 
-   var array=[]
-  
-   for(var i=0;i<palavras.length;++i)
-  
-       array.push(palavras[i].toUpperCase())
-  
-   return array
-  
-  }
-  console.log(transformaParaMaiusculo(["gato","cachorro","tartaruga"]))
 
+
+Exemplo:
+
+
+
+personagens = ["Hermione", "Trinity", "Leia"]
+filmes = ["Harry Potter", "Matrix", "Star wars"]
+lancamentos = [2001, 1999, 1977]
+id = 3
+
+filme(personagens, filmes, lancamentos, id)  
+// retorna a string "Leia é um personagem do filme Star wars que estreou no cinema em 1977."
+
+
+
+
+*/
+
+function filme(personagens, filmes, lancamentos, id){
+   if(id > 3 || id <1){
+      return "Essa não é uma opção válida."
+   }
+   else{
+      var a = personagens[id - 1] + " é um personagem do filme " + filmes[id-1] + " que estreou no cinema em " + lancamentos[id-1] + "."
+      return a
+   }
+}
+
+
+console.log(filme(["Hermione", "Trinity", "Leia"],["Harry Potter", "Matrix", "Star wars"],[2001, 1999, 1977],1))
