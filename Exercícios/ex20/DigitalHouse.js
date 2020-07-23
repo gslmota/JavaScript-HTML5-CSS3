d1e6b3e3f47060b63dcfffc1ac1ecf6f86550e5c
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-23 20:26:58 -0300

    Up

--- Exercícios/ex20/DigitalHouse.js
+++ Exercícios/ex20/DigitalHouse.js
@@ -1,3 +1,7 @@
 /*
 Crie um sistema para calcular a comissão dos vendedores de uma loja. Crie uma função que tenha dois argumentos: preço e porcentagem de comissão.
 */
+function comissao(preco, porcetagem){
+    var comissao = (preco * porcetagem)/100
+    return comissao
+}
